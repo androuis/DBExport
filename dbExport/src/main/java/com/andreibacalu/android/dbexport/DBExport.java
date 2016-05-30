c4ba26fcdfa5a8730b12daafc52ee0a46bc4a5ce
Andrei Bacalu <abacalu@EN65127.endava.net>
2016-05-30 15:04:16 +0300

    minor modifications

--- dbExport/src/main/java/com/andreibacalu/android/dbexport/DBExport.java
+++ dbExport/src/main/java/com/andreibacalu/android/dbexport/DBExport.java
@@ -2,6 +2,7 @@ package com.andreibacalu.android.dbexport;
 
 import android.content.Context;
 import android.os.Environment;
+import android.text.TextUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -14,13 +15,20 @@ import java.nio.channels.FileChannel;
 
 public class DBExport {
 
+    /**
+     * Exports a database from the application to the external storage.
+     *
+     * @param dbName the database to be exported
+     * @param exportedDbName the name of the file where the database is exported (can be null)
+     * @param context the Context from where the db is exported.
+     */
     public static void exportDatabse(String dbName, String exportedDbName, Context context) {
         try {
             File externalStorageDirectory = Environment.getExternalStorageDirectory();
 
             if (externalStorageDirectory.canWrite()) {
                 File currentDB = context.getDatabasePath(dbName);
-                File backupDB = new File(externalStorageDirectory, exportedDbName);
+                File backupDB = new File(externalStorageDirectory, TextUtils.isEmpty(exportedDbName) ? dbName : exportedDbName);
 
                 if (currentDB.exists()) {
                     FileChannel src = new FileInputStream(currentDB).getChannel();
